91cebf09b1ad8a9a4c302a6817e0d8bfe87289ac
Kevin Wooten <kevin@wooten.com>
2013-03-04 11:49:37 -0700

    Test with complex POJO mapping

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,55 +3,26 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import com.impossibl.postgres.BasicContext;
-import com.impossibl.postgres.system.Version;
-import com.impossibl.postgres.system.tables.PgType;
 
 
 public class Test {
 	
 	public static class RoleGrant {
 		
-		enum Role {
-			Tester,
-			Manager
-		}
-		
 		public UUID account_id;
-		public Role role;
-		public UUID getAccount_id() {
-			return account_id;
-		}
-		public void setAccount_id(UUID account_id) {
-			this.account_id = account_id;
-		}
-		public Role getRole() {
-			return role;
-		}
-		public void setRole(Role role) {
-			this.role = role;
-		}
+		public String role;
+		
 	}
 	
 	public static class Helper2 {
+		
 		public String node;
 		public RoleGrant rg;
-		public String getNode() {
-			return node;
-		}
-		public void setNode(String node) {
-			this.node = node;
-		}
-		public RoleGrant getRg() {
-			return rg;
-		}
-		public void setRg(RoleGrant rg) {
-			this.rg = rg;
-		}
+		
 	}
 
 	public static void main(String[] args) throws SQLException, IOException {
@@ -63,10 +34,16 @@ public class Test {
 			settings.put("username", "postgres");
 			settings.put("password", "test");
 			
-			BasicContext context = new BasicContext(socket, settings);
+			Map<String, Class<?>> targetTypeMap = new HashMap<String, Class<?>>();
+			targetTypeMap.put("helper2", Helper2.class);
+			targetTypeMap.put("role_granter", RoleGrant.class);
+			
+			BasicContext context = new BasicContext(socket, settings, targetTypeMap);
 			context.init();
 			
 			context.query("select array['A Name','cce010e0-8476-11e2-9e96-0800200c9a66','Tester']", HashMap.class);
+			context.query("select ('A Name',('cce010e0-8476-11e2-9e96-0800200c9a66'::uuid,'Tester'))::helper2", HashMap.class);
+			
 		}
 		
 	}
