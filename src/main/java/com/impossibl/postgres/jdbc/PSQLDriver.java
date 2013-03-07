1dd2592d96fd2e84e4d9566690f1d9f1a5196cdb
Kevin Wooten <kevin@wooten.com>
2013-03-06 22:01:14 -0700

    Clean up driver class imports

--- src/main/java/com/impossibl/postgres/jdbc/PSQLDriver.java
+++ src/main/java/com/impossibl/postgres/jdbc/PSQLDriver.java
@@ -8,8 +8,6 @@ import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
