a1c5f4e1c220e6c6e146ace5669acd5583361bf2
Mikko Tiihonen <mikko.tiihonen@nitor.fi>
2013-03-21 22:35:00 +0200

    Support unicode table and field names

--- src/main/java/com/impossibl/postgres/utils/ChannelBuffers.java
+++ src/main/java/com/impossibl/postgres/utils/ChannelBuffers.java
@@ -1,6 +1,7 @@
 package com.impossibl.postgres.utils;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import org.jboss.netty.buffer.ChannelBuffer;
 
@@ -16,9 +17,23 @@ public class ChannelBuffers {
 		return res;
 	}
 
+	public static String readCStringUtf8(ChannelBuffer buffer) {
+		byte[] bytes = new byte[buffer.bytesBefore((byte) 0) + 1];
+		buffer.readBytes(bytes);
+
+		String res = new String(bytes, 0, bytes.length-1, UTF_8);
+
+		return res;
+	}
+
 	public static void writeCString(ChannelBuffer buffer, String val) {
 		
 		buffer.writeBytes(val.getBytes(US_ASCII));
+		buffer.writeByte(0);
+	}
+
+	public static void writeCStringUtf8(ChannelBuffer buffer, String val) {
+		buffer.writeBytes(val.getBytes(UTF_8));
 		buffer.writeByte(0);
 	}
 
