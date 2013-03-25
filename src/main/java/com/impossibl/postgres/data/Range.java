30fe4d2559c5a3f04acee8608c30f6e71913f000
Kevin Wooten <kevin@wooten.com>
2013-03-24 19:10:48 -0700

    Fix range flags interpretation

--- src/main/java/com/impossibl/postgres/data/Range.java
+++ src/main/java/com/impossibl/postgres/data/Range.java
@@ -11,6 +11,8 @@ public class Range<T> {
 		private static final byte RANGE_UB_INC 	= 0x04;	/* upper bound is inclusive */
 		private static final byte RANGE_LB_INF 	= 0x08;	/* lower bound is -infinity */
 		private static final byte RANGE_UB_INF 	= 0x10;	/* upper bound is +infinity */
+		private static final byte RANGE_LB_NULL	= 0x20;	/* lower bound is null */
+		private static final byte RANGE_UB_NULL = 0x40;	/* upper bound is null */
 		
 		public Flags(byte value) {
 			super();
@@ -26,6 +28,10 @@ public class Range<T> {
 		}
 		
 		public boolean hasLowerBound() {
+			return (value & (RANGE_EMPTY | RANGE_LB_NULL | RANGE_LB_INF)) != 0;
+		}
+
+		public boolean isLowerBoundInclusive() {
 			return (value & RANGE_LB_INC) != 0;
 		}
 		
@@ -34,13 +40,17 @@ public class Range<T> {
 		}
 
 		public boolean hasUpperBound() {
+			return (value & (RANGE_EMPTY | RANGE_UB_NULL | RANGE_UB_INF)) != 0;
+		}
+
+		public boolean isUpperBoundInclusive() {
 			return (value & RANGE_UB_INC) != 0;
 		}
 
 		public boolean isUpperBoundInfinity() {
 			return (value & RANGE_UB_INF) != 0;
 		}
-
+		
 	}
 
 	Flags flags;
@@ -68,6 +78,15 @@ public class Range<T> {
 		return (T) values[0];
 	}
 	
+	public void setLowerBound(T val) {
+		values[0] = val;
+		if(val == null) {
+			flags.value |= Flags.RANGE_LB_NULL;
+			if(!hasUpperBound())
+				flags.value |= Flags.RANGE_EMPTY;
+		}
+	}
+	
 	public boolean isLowerBoundInfinity() {
 		return flags.isLowerBoundInfinity();
 	}
@@ -79,6 +98,15 @@ public class Range<T> {
 	@SuppressWarnings("unchecked")
 	public T getUpperBound() {
 		return (T) values[1];
+	}
+	
+	public void setUpperBound(T val) {
+		values[1] = val;
+		if(val == null) {
+			flags.value |= Flags.RANGE_UB_NULL;
+			if(!hasLowerBound())
+				flags.value |= Flags.RANGE_EMPTY;
+		}
 	}
 	
 	public boolean isUpperBoundInfinity() {
