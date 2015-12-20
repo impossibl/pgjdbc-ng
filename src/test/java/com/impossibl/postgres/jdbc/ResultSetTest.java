3c723d68abd261e2e657607283fd04677351d4fe
Kevin Wooten <kevin@wooten.com>
2015-12-20 08:22:46 -0700

    Fixed import munged during checkin

--- src/test/java/com/impossibl/postgres/jdbc/ResultSetTest.java
+++ src/test/java/com/impossibl/postgres/jdbc/ResultSetTest.java
@@ -55,6 +55,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
