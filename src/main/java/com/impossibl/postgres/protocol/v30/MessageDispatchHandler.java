c6794b6be5b4aa2207e64e8a20bd9d0fae2d0aef
Kevin Wooten <kevin@wooten.com>
2018-12-22 19:21:56 -0700

    Fix excess flushes from dispatch handler

--- src/main/java/com/impossibl/postgres/protocol/v30/MessageDispatchHandler.java
+++ src/main/java/com/impossibl/postgres/protocol/v30/MessageDispatchHandler.java
@@ -125,6 +125,7 @@ public class MessageDispatchHandler extends ChannelDuplexHandler {
     flushTrace();
     if (requiresFlush) {
       super.flush(ctx);
+      requiresFlush = false;
     }
   }
 
