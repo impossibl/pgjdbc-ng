f8b5fef55c3fae3f9ae5f40312aabf2c071f1784
Kevin Wooten <kevin@wooten.com>
2019-01-30 19:10:09 -0700

    Fix release publication
    Releases cannot be done by travis (due to signing requirement)

--- shared/src/build/publishing.gradle.kts
+++ shared/src/build/publishing.gradle.kts
@@ -7,13 +7,14 @@ apply {
 
 val version: String by project
 val isSnapshot: Boolean by project
+val isRelease = (project.properties["release"]?.toString() ?: "false").toBoolean()
 
 val repositoryUrl =
    URI.create(
-      if (isSnapshot)
-        "https://oss.sonatype.org/content/repositories/snapshots/"
-      else
+      if (isRelease)
         "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+      else
+        "https://oss.sonatype.org/content/repositories/snapshots/"
    )!!
 
 val organization: Map<String, Any> by project
@@ -22,84 +23,90 @@ val issuesUrl: String by project
 val scmUrl: String by project
 val scmGitUrl: String by project
 
-configure<PublishingExtension> {
+if (isSnapshot || isRelease) {
 
-  publications {
+  configure<PublishingExtension> {
 
-    register<MavenPublication>("maven") {
+    publications {
 
-      pom {
-        name.set(project.description)
+      register<MavenPublication>("maven") {
 
-        url.set(projectUrl)
+        pom {
+          name.set(project.description)
 
-        organization {
-          name.set(organization["name"] as String)
-          url.set(organization["url"] as String)
-        }
-
-        issueManagement {
-          system.set("GitHub")
-          url.set(issuesUrl)
-        }
-
-        licenses {
-          license {
-            name.set("Apache License 2.0")
-            distribution.set("repo")
-          }
-        }
-
-        scm {
           url.set(projectUrl)
-          connection.set(scmUrl)
-          developerConnection.set(scmGitUrl)
+
+          organization {
+            name.set(organization["name"] as String)
+            url.set(organization["url"] as String)
+          }
+
+          issueManagement {
+            system.set("GitHub")
+            url.set(issuesUrl)
+          }
+
+          licenses {
+            license {
+              name.set("Apache License 2.0")
+              distribution.set("repo")
+            }
+          }
+
+          scm {
+            url.set(projectUrl)
+            connection.set(scmUrl)
+            developerConnection.set(scmGitUrl)
+          }
+
+          developers {
+            developer {
+              id.set("kdubb0")
+              name.set("Kevin Wooten")
+            }
+            developer {
+              id.set("brettwooldridge")
+              name.set("Brett Wooldridge")
+            }
+            developer {
+              id.set("jesperpedersen")
+              name.set("Jesper Pedersen")
+            }
+          }
+
         }
 
-        developers {
-          developer {
-            id.set("kdubb0")
-            name.set("Kevin Wooten")
-          }
-          developer {
-            id.set("brettwooldridge")
-            name.set("Brett Wooldridge")
-          }
-          developer {
-            id.set("jesperpedersen")
-            name.set("Jesper Pedersen")
-          }
+        from(components["java"])
+
+        artifact(tasks.named<Jar>("sourcesJar").get())
+        artifact(tasks.named<Jar>("javadocJar").get())
+
+        tasks.findByName("uberJar")?.let {
+          artifact(it)
         }
 
-      }
-
-      from(components["java"])
-
-      artifact(tasks.named<Jar>("sourcesJar").get())
-      artifact(tasks.named<Jar>("javadocJar").get())
-
-      tasks.findByName("uberJar")?.let {
-        artifact(it)
       }
 
     }
 
-  }
-
-  repositories {
-    maven {
-      url = repositoryUrl
-      credentials {
-        username = project.findProperty("ossrhUsername") as? String ?: ""
-        password = project.findProperty("ossrhPassword") as? String ?: ""
+    repositories {
+      maven {
+        url = repositoryUrl
+        credentials {
+          username = project.findProperty("ossrhUsername") as? String ?: ""
+          password = project.findProperty("ossrhPassword") as? String ?: ""
+        }
       }
     }
+
   }
 
-}
+  if (isRelease) {
 
+    configure<SigningExtension> {
+      sign(the<PublishingExtension>().publications["maven"])
+    }
 
-configure<SigningExtension> {
-  isRequired = !isSnapshot && gradle.taskGraph.hasTask("publishMavenPublicationToMavenRepository")
-  sign(the<PublishingExtension>().publications["maven"])
+  }
+
 }
