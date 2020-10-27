a803ddc2fd7268d88b6f889029b8f35177ce9e82
Kevin Wooten <kevin@wooten.com>
2020-10-26 20:00:37 -0700

    Add PostgreSQL version 13 to default test matrix

--- driver/src/build/testing.gradle.kts
+++ driver/src/build/testing.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 apply { type(DockerComposePlugin::class) }
 
 
-val defaultPostgresVersions by extra("12, 11, 10, 9.6, 9.5, 9.4")
+val defaultPostgresVersions by extra("13, 12, 11, 10, 9.6, 9.5")
 
 
 val testTask = tasks.named<Test>("test") {
