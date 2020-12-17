f0f2f93d13fec5497b3b9b6fd5b75bc77b4025c5
Hai_tun <1346653001@qq.com>
2020-12-17 20:06:40 +0800

    Fix where localePart == "Chinese (Simplified)"

--- driver/src/main/java/com/impossibl/postgres/utils/Locales.java
+++ driver/src/main/java/com/impossibl/postgres/utils/Locales.java
@@ -148,7 +148,7 @@ public class Locales {
   private static void validateLocalePart(String localePart) {
     for (int i = 0; i < localePart.length(); i++) {
       char ch = localePart.charAt(i);
-      if (ch != ' ' && ch != '_' && ch != '-' && ch != '#' && !Character.isLetterOrDigit(ch)) {
+      if (ch != ' ' && ch != '_' && ch != '-' && ch != '#' && ch != '(' && ch != ')' && !Character.isLetterOrDigit(ch)) {
         throw new IllegalArgumentException(
             "Locale part \"" + localePart + "\" contains invalid characters");
       }
